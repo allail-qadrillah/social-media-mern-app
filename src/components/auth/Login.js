1e411f9f49458cd4054f4180c9cc5ae4989ced35
Muhammad Al lail Qadrillah <mallailqadrillah43@gmail.com>
2023-02-09 14:57:40 +0700

    setup route for auth

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import { Box, Center, Heading, FormControl, Input, FormLabel, FormErrorMessage, Button, Link, Text } from "@chakra-ui/react";
+import { REGISTER } from "../../lib/routes";
+import { Link as RouterLink } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <Center w='100%' h='100vh'>
+      <Box mx='1' maxW='md' p='9' borderWidth='1px' borderRadius='lg'>
+        <Heading mb='4' size='lg' textAlign='center'>Login</Heading>
+
+        <form onSubmit={() => { }}>
+          <FormControl isInvalid={true} py='2'>
+            <FormLabel>Email</FormLabel>
+            <Input type='email' placeholder="Email" />
+            <FormErrorMessage>email invalid</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={true} py='2'>
+            <FormLabel>Password</FormLabel>
+            <Input type='Password' placeholder="Password" />
+            <FormErrorMessage>Password invalid</FormErrorMessage>
+          </FormControl>
+
+          <Button mt='4' type="submit" colorScheme='purple' w='full' isLoading={true} loadingText='logging in'>Log in</Button>
+        </form>
+
+        <Text fontSize='xlg' align='center' mt='6'>
+          Don't have an account?{" "}
+          <Link as={RouterLink} to={REGISTER} color='purple.800' fontWeight='medium' textDecor='underline' _hover={{ color: 'purple.500' }}>Register</Link>
+          {" "}instead!
+        </Text>
+      </Box>
+    </Center>
   )
 }
--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,13 +1,22 @@
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
+import Layout from "../components/layout/index";
 import { createBrowserRouter } from "react-router-dom";
 
 export const ROOT = '/'
 export const LOGIN = '/login'
 export const REGISTER = '/register'
 
+export const PROTECTED = '/protected'
+export const DASHBOARD = `${PROTECTED}/dashboard`
+
 export const router = createBrowserRouter([
   { path: ROOT, element: 'ROOT' },
   { path: LOGIN, element: <Login /> },
-  { path: REGISTER, element: <Register/>},
+  { path: REGISTER, element: <Register /> },
+  { path: PROTECTED, 
+    element: <Layout />, 
+    children:[
+      { path: DASHBOARD, element: 'DASHBOARD' },
+    ]},
 ])
