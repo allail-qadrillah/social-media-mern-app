9dcb9ce6c71eb9b235d470691c1e3ac12c595b82
Muhammad Al lail Qadrillah <mallailqadrillah43@gmail.com>
2023-02-10 11:53:37 +0700

    create login funcionality

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,27 +1,42 @@
 import { Box, Center, Heading, FormControl, Input, FormLabel, FormErrorMessage, Button, Link, Text } from "@chakra-ui/react";
-import { REGISTER } from "lib/routes";
+import { useLogin } from "hooks/auth";
+import { DASHBOARD, REGISTER } from "lib/routes";
 import { Link as RouterLink } from "react-router-dom";
+import { useForm } from "react-hook-form"
+import { emailValidate, passwordValidate } from "utils/form-validate";
+
 
 export default function Login() {
+  const { login, isLoading } = useLogin()
+  const { register, handleSubmit, reset, formState: { errors } } = useForm()
+
+  async function handleLogin(data){
+    const succeeded =  await login( { email: data.email, 
+                                      password: data.password, 
+                                      redirectTo: DASHBOARD} )
+    if (succeeded) reset() // clear form input field if loggin succeded
+    
+  }
+
   return (
     <Center w='100%' h='100vh'>
       <Box mx='1' maxW='md' p='9' borderWidth='1px' borderRadius='lg'>
         <Heading mb='4' size='lg' textAlign='center'>Login</Heading>
 
-        <form onSubmit={() => { }}>
-          <FormControl isInvalid={true} py='2'>
+        <form onSubmit={( handleSubmit(handleLogin) )}>
+          <FormControl isInvalid={ errors.email } py='2'>
             <FormLabel>Email</FormLabel>
-            <Input type='email' placeholder="Email" />
-            <FormErrorMessage>email invalid</FormErrorMessage>
+            <Input type='email' placeholder="Email" {...register("email", emailValidate)}/>
+            <FormErrorMessage>{ errors.email && errors.email.message }</FormErrorMessage>
           </FormControl>
 
-          <FormControl isInvalid={true} py='2'>
+          <FormControl isInvalid={ errors.password } py='2'>
             <FormLabel>Password</FormLabel>
-            <Input type='Password' placeholder="Password" />
-            <FormErrorMessage>Password invalid</FormErrorMessage>
+            <Input type='Password' placeholder="Password" {...register("password", passwordValidate)}/>
+            <FormErrorMessage>{ errors.password && errors.password.message }</FormErrorMessage>
           </FormControl>
 
-          <Button mt='4' type="submit" colorScheme='purple' w='full' isLoading={true} loadingText='logging in'>Log in</Button>
+          <Button mt='4' type="submit" colorScheme='purple' w='full' isLoading={isLoading} loadingText='logging in'>Log in</Button>
         </form>
 
         <Text fontSize='xlg' align='center' mt='6'>
--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,12 +1,55 @@
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from 'lib/firebase'
+import { useState } from 'react'
+import { DASHBOARD } from 'lib/routes'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useToast } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
-export function useAuth(){
-  const [ authUser, isLoading, error ] = useAuthState( auth )
+export function useAuth() {
+  const [authUser, isLoading, error] = useAuthState(auth)
 
   return {
-    user : authUser,
+    user: authUser,
     isLoading,
     error
   }
+}
+
+export function useLogin() {
+  const [isLoading, setLoading] = useState(false)
+  const toast = useToast()
+  const navigate = useNavigate()
+
+  async function login({ email, password, redirectTo = DASHBOARD }) {
+    setLoading(true)
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+      toast({
+        title: 'You are logged in👍',
+        status: 'success',
+        isClosable: true,
+        position: 'top',
+        duration: 5000 
+      })
+      navigate(redirectTo)
+    }catch(error){
+      toast({
+        title: 'Logging in failed',
+        description: error.message,
+        status: 'error',
+        isClosable: true,
+        position: 'top',
+        duration: 5000 
+      })
+      setLoading(false)
+      return false; // returning false if loggin failed
+    }
+
+    setLoading(false)
+    return true; // retuning true if loggin succeeded
+  }
+
+  return { login, isLoading }
 }
